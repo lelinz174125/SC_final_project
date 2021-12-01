ca583f1b7d70717c3cd46f78f1ac5995e7ad359f
lelinz174125 <lelinz1217@gmail.com>
2021-12-01 13:27:24 -0500

    Data addressed

--- hf_prediction.py
+++ hf_prediction.py
@@ -19,16 +19,61 @@ df.head(10)
 
 '''
 Age: age of the patient [years]
-Sex: sex of the patient [M: Male, F: Female]
+Sex: sex of the patient [1: Male, 0: Female]
 ChestPainType: chest pain type [TA: Typical Angina, ATA: Atypical Angina, NAP: Non-Anginal Pain, ASY: Asymptomatic]
 RestingBP: resting blood pressure [mm Hg]
 Cholesterol: serum cholesterol [mm/dl]
 FastingBS: fasting blood sugar [1: if FastingBS > 120 mg/dl, 0: otherwise]
-RestingECG: resting electrocardiogram results [Normal: Normal, ST: having ST-T wave abnormality (T wave inversions and/or ST elevation or depression of > 0.05 mV), LVH: showing probable or definite left ventricular hypertrophy by Estes' criteria]
+RestingECG: resting electrocardiogram results [Normal: Normal, ST: having ST-T wave abnormality 
+            (T wave inversions and/or ST elevation or depression of > 0.05 mV), LVH: showing probable 
+            or definite left ventricular hypertrophy by Estes' criteria]
 MaxHR: maximum heart rate achieved [Numeric value between 60 and 202]
-ExerciseAngina: exercise-induced angina [Y: Yes, N: No]
+ExerciseAngina: exercise-induced angina [1: Yes, 0: No]
 Oldpeak: oldpeak = ST [Numeric value measured in depression]
 ST_Slope: the slope of the peak exercise ST segment [Up: upsloping, Flat: flat, Down: downsloping]
 HeartDisease: output class [1: heart disease, 0: Normal]
 '''
 
+new_df = pd.DataFrame(columns=['Age','Sex',	'ChestPainType_TA','ChestPainType_ATA','ChestPainType_ASY',
+                            'ChestPainType_NAP','RestingBP','Cholesterol','FastingBS','RestingECG_Normal',
+                            'RestingECG_ST','RestingECG_LVH','MaxHR','ExerciseAngina','Oldpeak','ST_Slope_Up',
+                            'ST_Slope_Flat','ST_Slope_Down','HeartDisease'])
+
+new_df['Age']=df['Age']
+new_df['Sex']=df['Sex']
+new_df['ChestPainType_TA']=df['ChestPainType']
+new_df['ChestPainType_ATA']=df['ChestPainType']
+new_df['ChestPainType_ASY']=df['ChestPainType']
+new_df['ChestPainType_NAP']=df['ChestPainType']
+new_df['RestingBP']=df['RestingBP']
+new_df['Cholesterol']=df['Cholesterol']
+new_df['FastingBS']=df['FastingBS']
+new_df['RestingECG_Normal']=df['RestingECG']
+new_df['RestingECG_ST']=df['RestingECG']
+new_df['RestingECG_LVH']=df['RestingECG']
+new_df['MaxHR']=df['MaxHR']
+new_df['ExerciseAngina']=df['ExerciseAngina']
+new_df['Oldpeak']=df['Oldpeak']
+new_df['ST_Slope_Up']=df['ST_Slope']
+new_df['ST_Slope_Flat']=df['ST_Slope']
+new_df['ST_Slope_Down']=df['ST_Slope']
+new_df['HeartDisease']=df['HeartDisease']
+
+new_df['Sex'].replace(['M','F'],[1.0, 0.0], inplace = True)
+new_df['ChestPainType_TA'].replace(['TA','ATA','ASY','NAP'],[1.0, 0.0, 0.0, 0.0], inplace = True)
+new_df['ChestPainType_ATA'].replace(['TA','ATA','ASY','NAP'],[0.0, 1.0, 0.0, 0.0], inplace = True)
+new_df['ChestPainType_ASY'].replace(['TA','ATA','ASY','NAP'],[0.0, 0.0, 0.1, 0.0], inplace = True)
+new_df['ChestPainType_NAP'].replace(['TA','ATA','ASY','NAP'],[0.0, 0.0, 0.0, 1.0], inplace = True)
+new_df['RestingECG_Normal'].replace(['Normal','ST','LVH'],[1.0, 0.0, 0.0], inplace = True)
+new_df['RestingECG_ST'].replace(['Normal','ST','LVH'],[0.0, 1.0, 0.0], inplace = True)
+new_df['RestingECG_LVH'].replace(['Normal','ST','LVH'],[0.0, 0.0, 1.0], inplace = True)
+new_df['ExerciseAngina'].replace(['Yes','No'],[1.0, 0.0], inplace = True)
+new_df['ST_Slope_Up'].replace(['Up','Flat','Slop'],[1.0, 0.0, 0.0], inplace = True)
+new_df['ST_Slope_Flat'].replace(['Up','Flat','Slop'],[0.0, 1.0, 0.0], inplace = True)
+new_df['ST_Slope_Down'].replace(['Up','Flat','Slop'],[0.0, 0.0, 1.0], inplace = True)
+
+new_df.info()
+new_df.head(10)
+print(new_df)
+
+new_df.to_csv('heart_data_addressed.csv')
